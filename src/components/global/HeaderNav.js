3be5c3ec8fb8ab85ee6f93a40e937edd5edee115
Lucas Alvarado <lucasalvarado@Lucass-MacBook-Pro.local>
2018-08-16 10:53:12 -0400

    categories

--- src/components/global/HeaderNav.js
+++ src/components/global/HeaderNav.js
@@ -7,7 +7,7 @@ const HeaderNav = () => (
   <div className="nav-container">
     <nav className="primary-nav light">
       <Link to="/products">Products</Link>
-      <Link to="/styles">Styles</Link>
+      <Link to="/styles">Categories</Link>
     </nav>
     <nav className="secondary-nav light">
       <CartCounter />
